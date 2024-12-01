79dc4c340d745938319d05593ab09e87ad7c2be1
fahmon4456 <94559190+fahmon4456@users.noreply.github.com>
2024-12-01 20:58:48 +0300

    Revert "i"
    This reverts commit bd144e9c82b611ac0ab597e8e25fa15c45faa845.

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function myFunction() {
     document.getElementById("c").checked = false;
     console.log("f");
 }
-side = document.getElementById("sidebar");
-function cli() {
-   side.style.color = "blue";
-   console.log("f");
-}
